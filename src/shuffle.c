0d28d664d5f5cafe71ae733ee2f2d3a0ca502817
Albert «Northsoft» Rayanov <northsoft.root@gmail.com>
2018-04-29 20:30:01 +0700

    Added shuffle function

---
+++ src/shuffle.c
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <stdlib.h>
+
+void shuffle (int* puzzle, size_t side_length)
+{
+	srand(time(NULL));
+	size_t row_num, col_num, a, b, tmp;
+	for (row_num = 0; row_num < side_length; ++row_num){
+		for (col_num = 0; col_num < side_length; ++j){
+			a = rand () % (side_length - 1);
+			b = rand () % (side_length - 1);
+			tmp = puzzle[4*row_num+col_num];
+			puzzle[4*row_num+col_num] = puzzle[4*a+b];
+			puzzle[4*a+b] = tmp;
+		}
+	}
+}
