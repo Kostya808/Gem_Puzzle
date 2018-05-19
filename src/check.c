be3ddd97801f27314bf9b873360560d4ed7e1010
Albert «Northsoft» Rayanov <northsoft.root@gmail.com>
2018-05-19 23:12:45 +0700

    Fixed check bugs

--- src/check.c
+++ src/check.c
@@ -6,14 +6,16 @@ bool check (int* puzzle, unsigned int side_length)
 	size_t row_num, col_num, e, k, N = 0, add = 0;
 	for (row_num = 0; row_num < side_length; ++row_num){
 		for (col_num = 0; col_num < side_length; ++col_num){
-			if (puzzle[side_length*row_num+col_num] == 0)
+			if (puzzle[side_length*row_num+col_num] == 0){
 				e = row_num + 1;
-			do {
+				continue;
+			}
+			while (puzzle[side_length*row_num+col_num] < puzzle[side_length*row_num+col_num+add]) {
 				++k;
 				++add;
 				if ((row_num == side_length - 1)&&(col_num == side_length - 1))
 					k = 0;
-			} while (puzzle[side_length*row_num+col_num] < puzzle[side_length*row_num+col_num+add]);
+			}
 			N += k;
 			k = 0;
 			add = 0;
