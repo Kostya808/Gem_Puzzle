ebec0d13b0359b6827c865418a2790d199bb8b3f
KoSasha <sasha_pishi99@mail.ru>
2018-05-30 14:48:53 +0200

    Writing a new record

--- src/function.h
+++ src/function.h
@@ -11,6 +11,7 @@ void shuffle (int* puzzle, unsigned int side_length);
 bool check (int* puzzle, unsigned int side_length);
 void interface(int size, int * puzzle);
 size_t move_cells(size_t empty, size_t size, int *puzzle);
+int record(size_t size, int move);
 int search(int *puzzle, size_t n, int key);
 void swap(int *a, int *b);
 int check_win(size_t size, int *puzzle);
--- src/win.c
+++ src/win.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include <stdio.h>
+#include "function.h"
 
 int check_win(size_t size, int *puzzle)
 {
@@ -14,5 +16,38 @@ int check_win(size_t size, int *puzzle)
     if (g == size * size - 1){
         return 1;
     }
+    return 0;
+}
+
+int record(size_t size, int move) {
+    int n = 0, count, i;
+    char ch = '\n';
+    FILE *in = fopen("record.txt", "r");
+    if (in == NULL) {
+        return 2;
+    }
+    while (!feof(in)) {
+        if (fgetc(in) == '\n') {
+            n++;
+            printf("%d", n);
+        }
+    }
+    fseek(in , 0, SEEK_SET);
+    int records[n];
+    for (count = 0; count < n; count++) {
+        fscanf(in, "%d", &records[count]);
+    }
+    fclose(in);
+    FILE *out = fopen("record.txt", "w");
+    if (out == NULL) {
+        return 3;
+    }
+    if ((records[size - 2] != 0 && records[size - 2] > move) || (records[size - 2] == 0)) {
+        records[size - 2] = move;
+    }
+    for (i = 0; i < n; i++) {
+        fprintf (out, "%d%c", records[i], ch);
+    }
+    fclose(out);
     return 0;
 }
